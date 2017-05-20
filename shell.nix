5f97eb706a9ddbfd403d1bc9354a51c6866d1787
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-05-20 17:49:35 +0300

    shell.nix:  bump nixpkgs to tip of NixOS 17.03;  adapt to changes

--- shell.nix
+++ shell.nix
@@ -16,7 +16,6 @@ ghc       = ghcOrig.override (oldArgs: {
   let parent = (oldArgs.overrides or (_: _: {})) new old;
   in with new; parent // {
       intero         = overhub  old.intero "commercialhaskell/intero" "e546ea086d72b5bf8556727e2983930621c3cb3c" "1qv7l5ri3nysrpmnzfssw8wvdvz0f6bmymnz1agr66fplazid4pn" { doCheck = false; };
-      turtle_1_3_0   = doJailbreak old.turtle_1_3_0;
     };
   });
 
@@ -25,7 +24,7 @@ ghc       = ghcOrig.override (oldArgs: {
 ###
 drvf =
 { mkDerivation, stdenv, src ? ./.
-, base, turtle_1_3_0, cassava, vector, safe, aeson, yaml, lens-aeson
+, base, turtle, cassava, vector, safe, aeson, yaml, lens-aeson
 }:
 mkDerivation {
   pname = "iohk-nixops";
@@ -35,7 +34,7 @@ mkDerivation {
   isExecutable = true;
   doHaddock = false;
   executableHaskellDepends = [
-   base turtle_1_3_0 cassava vector safe aeson yaml lens-aeson
+   base turtle cassava vector safe aeson yaml lens-aeson
   ];
   # description  = "Visual mind assistant";
   license      = stdenv.lib.licenses.agpl3;
